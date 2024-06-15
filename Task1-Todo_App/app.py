33c9c53a341f8823ad4723dc32ca9259803c955d
Kayathry <iamgayathrivijayakumar@gmail.com>
2024-06-15 16:37:29 +0530

    Added description and with context

--- Task1-Todo_App/app.py
+++ Task1-Todo_App/app.py
@@ -10,7 +10,12 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
+    description = db.Column(db.String(255))
     complete = db.Column(db.Boolean)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
@@ -22,18 +27,36 @@ def index():
 @app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
+    description = request.form.get("description")
+    new_todo = Todo(title=title, description=description, complete=False)
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
 
 
-@app.route("/update/<int:todo_id>")
-def update(todo_id):
+@app.route("/change_complete_status/<int:todo_id>")
+def change_complete_status(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
+
+
+@app.route("/update/<int:todo_id>", methods=["POST"])
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.id = todo_id
+    todo.title = request.form.get("title")
+    todo.description = request.form.get("description")
+    todo.complete = todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/edit/<int:todo_id>")
+def edit(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    return render_template('edit.html', todo=todo)
 
 
 @app.route("/delete/<int:todo_id>")
@@ -41,9 +64,11 @@ def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
 
 
 if __name__ == '__main__':
-    db.create.all()
     app.run(debug=True)
+
+
+
